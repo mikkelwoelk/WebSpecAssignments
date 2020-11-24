439aee7b8d0e4a9164356b7348187d7c7752cbfa
Mikkel Wølk <mikkelwolk@gmail.com>
2020-11-24 11:57:41 +0100

    made user input into numbers

--- assJS7.0/js.js
+++ assJS7.0/js.js
@@ -1,14 +1,14 @@
 const doThis = function () {
-    let firstNum = window.prompt('write a number');
-    let secondNum = window.prompt('write another number');
-    let thirdNum = window.prompt('write one more number');
+    let firstNum = Number(prompt('write a number'));
+    let secondNum = Number(prompt('write another number'));
+    let thirdNum = Number(prompt('write one more number'));
 
     if (firstNum > secondNum && thirdNum){
-        console.log('1st is the highest number:' + ' ' + firstNum);
+        console.log('1st is the highest number with:' + ' ' + firstNum);
     } else if (secondNum > thirdNum && firstNum){
-        console.log('2nd is the highest number:' + ' ' + secondNum);
+        console.log('2nd is the highest number with:' + ' ' + secondNum);
     } else {
-        console.log('3rd is the highest number:' + ' ' + thirdNum);
+        console.log('3rd is the highest number with:' + ' ' + thirdNum);
     }
 }
 
